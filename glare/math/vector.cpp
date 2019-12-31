c78d66cb9fce0264d5556afc78637135ffed2422
Krovma <krovma726@gmail.com>
2019-12-31 12:28:09 -0600

    setup test

--- glare/math/vector.cpp
+++ glare/math/vector.cpp
@@ -1,1 +1,13 @@
 #include "glare/math/vector.h"
+
+////////////////////////////////
+namespace glare {
+
+vec2& vec2::operator+=(const vec2& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+	return *this;
+}
+
+};
--- glare/math/vector.h
+++ glare/math/vector.h
@@ -2,8 +2,12 @@
 #include "glare/core/common.h"
 namespace glare
 {
-class vec2
+struct vec2
 {
-	
+	union { float32 x = 0.f; float32 u; float32 s; };
+	union { float32 y = 0.f; float32 v; float32 t; };
+	vec2() = default;
+
+	vec2& operator += (const vec2& rhs);
 };
 };
